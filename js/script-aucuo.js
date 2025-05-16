038c418cd69d730a719126f0506955082049bec7
aucuo <jahor.sykaviec@gmail.com>
2025-05-17 02:31:26 +0300

    Updates

--- js/script-aucuo.js
+++ js/script-aucuo.js
@@ -3,7 +3,6 @@ const articleNavs = document.querySelectorAll(".article-nav");
 
 articleNavs.forEach(el => {
     const links = el.querySelectorAll("a.article-nav__link");
-    const body = el.querySelector("article-nav__body");
 
     links.forEach(link => {
         link.addEventListener('click', ()=>{
